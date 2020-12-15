734810669f2a28bafad20f27c540367b7fde1fe7
cheung-l <71692699+cheung-l@users.noreply.github.com>
2020-12-15 15:31:23 +0800

    Create 9-6Circle.java

---
+++ 9-6Circle.java
@@ -0,0 +1,24 @@
+public class Circle {
+
+	/**The radius of the circle */
+	double radius;
+	
+	/** The number of objects created */
+	static int numberOfObject = 0;//统计创建的Circle类的对象个数
+	
+	/** Construct a circle with radius 1 */
+	Circle(){
+		radius = 1;
+		numberOfObject++;
+	}
+	
+	/**Return numberOfObject */
+	static int getNumberOfObjects() {
+		return numberOfObject;
+	}
+	double getArea() {
+		return radius * radius * Math.PI;
+	
+	}
+	
+}
